e63a10255773c08744d4a7785886674212ca7e7f
Raaulsthub <rsteinmetz@inf.ufsm.br>
2023-05-08 09:33:44 -0300

    better tests

--- test_ppo.py
+++ test_ppo.py
@@ -14,8 +14,8 @@ if __name__ == '__main__':
     # tweak to discover test configuration
     agent = Agent(n_actions=env.action_space.n, 
                     input_dims=env.observation_space.shape,
-                    gamma=0.99, alpha=0, gae_lambda=0.95,
-                    policy_clip=0.2, batch_size=256, n_epochs=10)
+                    gamma=0.99, alpha=0, gae_lambda=1,
+                    policy_clip=1, batch_size=256, n_epochs=10)
     
     agent.load_models()
 
@@ -37,7 +37,7 @@ if __name__ == '__main__':
         scores.append(score)
 
 
-    data = {'episode': np.arange(len(scores)), 'avg_reward': score}
+    data = {'episode': np.arange(len(scores)), 'avg_reward': scores}
     df = pd.DataFrame(data)
     df.to_csv('./testing_log/ppo/lunar_lander.csv', index=False)
 
